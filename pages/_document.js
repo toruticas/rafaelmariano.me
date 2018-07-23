1b9f457c7f19deb6e0e06ad882792ef3781bd204
Rafael <rafael.silva@arquivei.com.br>
2018-07-22 21:50:18 -0300

    Update profile image and add GA
    Previouis profile has 500kb. This image converted and resized to 76kb.
    Google Analytics snippet append to the body.

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,8 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106419274-1"/>
+          <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-106419274-1');` }} />
         </body>
       </html>
     )
